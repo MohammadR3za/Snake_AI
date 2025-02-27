0f46e473e5e793f2f89f01c8b507dfc939ed9b73
mohammadr3za <mohammadrezam6731@gmail.com>
2025-02-27 07:54:00 +0330

    refined the ai and the game

---
+++ src/build/CMakeFiles/snake_ai.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mrm/snake_ai/src/main.cpp" "CMakeFiles/snake_ai.dir/main.cpp.o" "gcc" "CMakeFiles/snake_ai.dir/main.cpp.o.d"
+  "/home/mrm/snake_ai/src/nn.cpp" "CMakeFiles/snake_ai.dir/nn.cpp.o" "gcc" "CMakeFiles/snake_ai.dir/nn.cpp.o.d"
+  "/home/mrm/snake_ai/src/snake.cpp" "CMakeFiles/snake_ai.dir/snake.cpp.o" "gcc" "CMakeFiles/snake_ai.dir/snake.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/CMakeFiles/snake_ai.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_ai.dir/main.cpp.o"
+  "CMakeFiles/snake_ai.dir/main.cpp.o.d"
+  "CMakeFiles/snake_ai.dir/nn.cpp.o"
+  "CMakeFiles/snake_ai.dir/nn.cpp.o.d"
+  "CMakeFiles/snake_ai.dir/snake.cpp.o"
+  "CMakeFiles/snake_ai.dir/snake.cpp.o.d"
+  "snake_ai"
+  "snake_ai.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake_ai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
